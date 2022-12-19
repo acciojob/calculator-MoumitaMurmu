7308acd3c3b01454a85b57427f8db8b36ba1a1d1
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2022-12-19 21:31:57 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your code here
+var allButtons = document.querySelectorAll("button");
+var input = document.querySelector("#input");
+
+for (const button of allButtons) {
+  button.addEventListener("click", changeInput);
+}
+
+function changeInput(event) {
+  let clickedText = event.target.innerText;
+
+  if (clickedText == "AC") {
+    input.value = "";
+  } else if (clickedText == "=") {
+    input.value = eval(input.value);
+  } else {
+    input.value += clickedText;
+  }
+}
